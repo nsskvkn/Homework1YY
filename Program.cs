857e9ebc4f784f1ea6677fedabfe2e5062207e79
nsskvkn <d9127506@gmail.com>
2025-04-21 18:11:43 +0300

    did the homework

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Homework;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var arr = new SimpleArray(5);
+            Console.WriteLine("Довжина = " + arr.Length);
+
+            arr[2] = 10;
+            Console.WriteLine("arr[2] = " + arr[2]);
+
+            arr[10] = 5;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+}
---
+++ SafeArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework
+{
+    public class SafeArray
+    {
+        private int[] data;
+
+        public SafeArray(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Розмір масиву > 0");
+            data = new int[size];
+        }
+
+        public int Length
+        {
+            get => data.Length;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Розмір масиву > 0");
+
+                Array.Resize(ref data, value);
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                    throw new IndexOutOfRangeException($"Індекс {index} виходить за межі масиву");
+                return data[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Length)
+                    throw new IndexOutOfRangeException($"Індекс {index} виходить за межі масиву");
+                data[index] = value;
+            }
+        }
+    }
+}
